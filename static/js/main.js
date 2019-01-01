916363db28c6b60268077b3e8d19b38b9a7be7c8
Dj Walker-Morgan <dj@Crow-2.local>
2019-01-01 11:27:43 +0000

    Small changes ongoing

--- static/js/main.js
+++ static/js/main.js
@@ -2,15 +2,20 @@
 const baseurl = "http://localhost:3000/";
 
 window.addEventListener("load", () => {
-  const el = $("#nextevent");
+  const nextEventTitle = $("#nextevent-title");
+  const nextEventDate= $("#nextevent-date");
+  const nextEventDesc= $("#nextevent-desc");
+
     console.log("Getting");
   fetch(baseurl + "events/next")
     .then(data => {
      return data.json();
     })
     .then(myEvent => {
-      console.log(JSON.stringify(myEvent));
-      el.html(myEvent.description)
+      console.log(JSON.stringify(myEvent,null," "));
+      nextEventTitle.html(myEvent.name);
+      nextEventDate.html(myEvent.date)
+      nextEventDesc.html(myEvent.description)
     })
     .catch(error => {
       console.log(error);
